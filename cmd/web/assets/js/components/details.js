cdcb93bdacebff554817c7a7c08fac4a872e1743
Vitor Rodrigues <vpaulo@users.noreply.github.com>
2024-09-06 11:25:05 +0100

    added details component to load quiljs

---
+++ cmd/web/assets/js/components/details.js
@@ -0,0 +1,24 @@
+class DetailsElement extends HTMLElement {
+  connectedCallback() {
+    const task_id = this.getAttribute("task-id");
+    console.log("task id : ", task_id);
+
+    // TODO: configure
+    const quill = new Quill("#note-editor", {
+      modules: {
+        toolbar: [
+          [{ header: [1, 2, 3, false] }],
+          ["bold", "italic", "underline", "strike"],
+          ["blockquote", "code-block"],
+          ["link", "image"],
+          [{ list: "ordered" }, { list: "bullet" }],
+        ],
+      },
+      bounds: this,
+      placeholder: "Add description...",
+      theme: "snow",
+    });
+  }
+}
+
+customElements.define("rm-details", DetailsElement);
