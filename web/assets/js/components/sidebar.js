44b6d78f644dd96957da51f68fedb634a3225eb2
Vitor Rodrigues <vpaulo@users.noreply.github.com>
2025-03-10 20:14:39 +0000

    29: initial changes for adding landing page

--- web/assets/js/components/sidebar.js
+++ web/assets/js/components/sidebar.js
@@ -32,7 +32,7 @@ class SidebarElement extends HTMLElement {
     form.reset();
 
     form.removeAttribute("hx-put");
-    form.setAttribute("hx-post", "/lists/create");
+    form.setAttribute("hx-post", "/v0/lists/create");
 
     form.querySelectorAll("input:checked")?.forEach((el) => {
       el.removeAttribute("checked");
