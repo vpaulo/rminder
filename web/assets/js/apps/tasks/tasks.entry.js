a3564b981c231de9ba89d1c19116807248e5325b
Vitor Rodrigues <vpaulo@users.noreply.github.com>
2026-03-07 17:24:59 +0000

    Added build script for JS

---
+++ web/assets/js/apps/tasks/tasks.entry.js
@@ -0,0 +1,6 @@
+import "../../components/details.js";
+import "../../components/sidebar.js";
+import "../../components/listNavigation.js";
+import "../../components/appSwitcher.js";
+import "../../components/settingsSwitcher.js";
+import "./tasks.js";
